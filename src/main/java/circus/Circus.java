8c7340614e7c5c81ebfb61e38c8829fcb03a0c04
ashleyang2001 <ashleyang2001@live.com>
2025-02-16 22:55:01 +0800

    Update code to use ArrayList

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -4,9 +4,12 @@ import circus.animal.Animal;
 import circus.animal.Duck;
 import circus.animal.Parrot;
 import circus.animal.Tiger;
+import circus.animal.Elephant;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Circus {
     private static Animal[] animals = {
@@ -41,8 +44,30 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        makeAnimalsTalk();
-        System.out.println("Total value of animals " + calculateAssetValue(animals));
-        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+        System.out.println(animals.length);
+//        makeAnimalsTalk();
+//        System.out.println("Total value of animals " + calculateAssetValue(animals));
+//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+
+        Parrot perry = new Parrot("Perry");
+        animalArrayList.add(perry);
+        animalArrayList.add(new Elephant("Brute"));
+        Duck andy = new Duck("Andy");
+        animalArrayList.add(andy);
+
+        // Substitutability allows to add specific animals to Animal ArrayList
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
+        System.out.println(animalArrayList.size());
+
+        System.out.println("Perry is in position: " + animalArrayList.indexOf(perry));
+
+        animalArrayList.sort(Animal.AnimalNameComparator);
+        System.out.println("After Sorting:");
+        for (Animal a : animalArrayList) {
+            System.out.println(a);
+        }
     }
 }
